ec3d0e527761f75275dd3bc6e11588ad4be71a99
waready <elbarto119@hotmail.com>
2019-07-05 17:39:36 -0500

    50 % de avance selecion de minas

--- src/components/Cuadro.vue
+++ src/components/Cuadro.vue
@@ -1,3 +1,32 @@
 <template>
-    
+    <div class="cuadro cuadro-inicial">
+        <span>{{info.valor}}</span>
+    </div>
 </template>
+<script>
+export default {
+    props:['info'],
+
+}
+</script>
+
+<style>
+    .cuadro{
+        display:grid;
+        justify-items:center;
+        align-items:center;
+    }
+    .cuadro-inicial{
+        width:20px;
+        height:20px;
+        background-color: #bdbdbd;
+        border-top-color: #fff;
+        border-left-color: #fff;
+        border-right-color: #818181;
+        border-bottom-color:#818181;
+        border-style: solid;
+        border-width:3px;
+        cursor:pointer;
+    }
+
+</style>
--- src/components/Tablero.vue
+++ src/components/Tablero.vue
@@ -2,11 +2,11 @@
     <div class="tableto">
         <div class="niveles">
             <span>Nivel:</span>
-            <span class="nivel">1</span>
-            <span class="nivel">2</span>
-            <span class="nivel">3</span>
+            <span @click="selecionarNivel(1)" class="nivel" :class="{'nivel-seleccionado': nivelActual.nivel == 1}">1</span>
+            <span @click="selecionarNivel(2)" class="nivel" :class="{'nivel-seleccionado': nivelActual.nivel == 2}">2</span>
+            <span @click="selecionarNivel(3)" class="nivel" :class="{'nivel-seleccionado': nivelActual.nivel == 3}">3</span>
         </div>
-        <div class="panel">
+        <div class="panel"> 
             <div class="marcador minas-restantes">
                 999     
             </div>
@@ -18,30 +18,137 @@
             </div>
         </div>
         <div class="matriz">
-
+            <Cuadro v-for="(item ,index) in cuadros" :key="index" 
+            :style= " 'grid-row: ' + item.fila + '; grid-column: '+ item.columna + ';'"
+            :info = "item"></Cuadro>
         </div>
     </div>
 </template>
 <script>
+import Cuadro from './Cuadro.vue'
 export default {
+    components:{
+        Cuadro,
+    },
+    created() {
+        this.nivelActual = this.nivelPrincipiante
+        this.iniciarNivel();
+    },
+    data() {
+        return {
+            cuadros: [],
+            nivelPrincipiante:{
+                nivel: 1,
+                filas: 9,
+                columnas:9,
+                minas:10
+            },
+            nivelIntermedio:{
+                nivel: 2,
+                filas: 16,
+                columnas:16,
+                minas:40
+            },
+            nivelExperto:{
+                nivel: 3,
+                filas: 16,
+                columnas:30,
+                minas:99
+            },
+            nivelActual: null,
+            minas:[],
+
+        }
+    },
+    methods: {
+        selecionarNivel(nivel){
+            if(this.nivelActual.nivel == nivel){
+                return;
+            }
+            if(nivel == 1){
+                this.nivelActual = this.nivelPrincipiante
+                
+            }
+            else if(nivel == 2){
+
+                this.nivelActual = this.nivelIntermedio
+                  
+            } 
+            else{
+                this.nivelActual = this.nivelExperto
+                      
+            }
+            this.iniciarNivel()
+
+        },
+        iniciarNivel(){
+            let filas = this.nivelActual.filas;
+            let columnas= this.nivelActual.columnas;
+            let totalCuadros = filas * columnas;
+
+            this.cuadros = [];
+            let indices = [];
+
+            for( let i = 0; i< totalCuadros; i++){
+                let cuadro = {
+                    valor: '',
+                    fila: Math.floor( i/columnas) +1 ,
+                    columna: (i % columnas) +1
+                }
+             
+                this.cuadros.push(cuadro)
+                indices.push(i)
+            }
+            for(let i=0;i<this.nivelActual.minas;i++){
+                let posicion  = Math.floor(Math.random()*(indices.length - 1))
+                let indice = indices[posicion]
+                this.cuadros[indice].valor = '💣'
+                this.minas.push(this.cuadros[indice])
+
+                indices.splice(posicion, 1)
+            }
+
+        }
+    },
     
 }
 </script>
 <style>
+@import url('https://fonts.googleapis.com/css?family=Roboto+Mono&display=swap');
+.html{
+    font-family: 'Roboto Mono', monospace;
+}
     .tableto{
         display: grid;
         justify-content: center;
-        background-color: rgb(121, 136, 121);
+        background-color: #bdbdbd;
         padding: 10px;
     }
     .niveles{
         display: grid;
         grid-auto-flow: column;
+        grid-gap:10px;
+        font-size:24px;
+        padding:5px;
+        justify-content:start;
+        align-items:center;
 
     }
     .nivel{
         width: 32;
         height: 32;
+        color:#5c5c5c;
+        text-align: center;
+        vertical-align: center;
+        cursor: pointer;
+    }
+    .nivel-seleccionado{
+        color: #fff !important;
+        background-color: #5f9cff;
+        border-style:solid;
+        border-width: 2px;
+        border-radius: 50%;
+        cursor:default; 
     }
     .panel{
         display: grid;
@@ -70,8 +177,7 @@ export default {
     .matriz{
         display: grid;
         background-color: #7b7b7b;
-        width: 400px;
-        height: 200px;
+
     }
 
 </style>
